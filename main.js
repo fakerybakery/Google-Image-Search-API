60f06b64c9a8c5f32bbdf190319a782b195184be
mrfakename <me@mrfake.name>
2022-11-16 14:49:05 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const jsdom = require("jsdom");
+
+function searchImage(query) {
+    query = encodeURIComponent(query);
+    var options = {
+        headers: new Headers({
+            'X-Requested-With': 'XMLHttpRequest'
+        }),
+    }
+    return new Promise(function(resolve, reject) {
+        fetch("https://cors-anywhere.herokuapp.com/https://www.google.com/search?source=lnms&sa=X&gbv=1&tbm=isch&q=" + query, options)
+            .then((res) => res.text())
+            .then((res) => {
+                var p = new jsdom.JSDOM(res);
+                var parser = p.window.document;
+                var table = parser.querySelectorAll('table')[2];
+                var imgs = table.querySelectorAll('img');
+                var imageList = [];
+                imgs.forEach((img) => {
+                    imageList.push(img.src);
+                });
+                resolve(imageList);
+            })
+            .catch((err) => console.log(err));
+    });
+}
