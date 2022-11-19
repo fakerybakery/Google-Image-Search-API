01c20e703b0e9c9c8b83d36a0e00eac131d3c6ee
mrfakename <me@mrfake.name>
2022-11-18 16:30:21 -0800

    - Add caching  - Add option to specify how many results!

--- main.php
+++ main.php
@@ -1,20 +1,46 @@
 <?php
-function searchImage($query) {
-    $query = urlencode($query);
-    $contents = @file_get_contents('https://www.google.com/search?source=lnms&sa=X&gbv=1&tbm=isch&q=' . $query);
-    if ($contents == false) {
-        echo 'Error, likely 404.';
-        echo "\n";
-    } else {
-        $dom = new DOMDocument();
-        $dom->loadHTML($contents);
-        $dom->preserveWhiteSpace = false;
-        $table = $dom->getElementsByTagName('table')[2];
-        $imgs = $table->getElementsByTagName('img');
-        $imglist = [];
-        foreach ($imgs as $img) {
-            array_push($imglist, $img->getAttribute('src'));
-        }
-        return $imglist;
+function fgc($url, $prefix = '') {
+    if (!is_dir('cache')) {
+        mkdir('cache');
     }
+    $cache_file = 'cache/' . md5($url) . $prefix;
+    if (file_exists($cache_file)) {
+        if(time() - filemtime($cache_file) > 3600) {
+            $cache = @file_get_contents($url);
+            file_put_contents($cache_file, $cache);
+        } else {
+            $cache = @file_get_contents($cache_file);
+        }
+    } else {
+        $cache = @file_get_contents($url);
+        file_put_contents($cache_file, $cache);
+    }
+    return $cache;
+}
+
+function searchImage($query, $numResults = 20) {
+    $numResults = intval($numResults);
+    $num = ceil(floatval($numResults / 20));
+    $imglist = [];
+    foreach (range(1, $num) as $n) {
+        $query = urlencode($query);
+        $start = ($n - 1) * 20;
+        $url = 'https://www.google.com/search?source=lnms&sa=X&gbv=1&tbm=isch&q=' . $query . '&start=' . $start;
+        $contents = @fgc($url, '.html');
+        if ($contents == false) {
+            //Err
+        } else {
+            $dom = new DOMDocument();
+            $dom->loadHTML($contents);
+            $dom->preserveWhiteSpace = false;
+            $table = $dom->getElementsByTagName('table')[2];
+            $imgs = $table->getElementsByTagName('img');
+            foreach ($imgs as $img) {
+                $url = $img->getAttribute('src');
+                @fgc($url, '.jpg');
+                array_push($imglist, 'cache/' . md5($url) . '.jpg');
+            }
+        }
+    }
+    return array_slice($imglist, 0, $numResults);
 }
