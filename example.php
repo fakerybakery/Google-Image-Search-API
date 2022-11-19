270655ca6314dbdba49165460580be5d3268fef1
mrfakename <me@mrfake.name>
2022-11-18 16:04:34 -0800

    Create example.php

---
+++ example.php
@@ -0,0 +1,23 @@
+<?php
+function searchImage($query) {
+    $query = urlencode($query);
+    $contents = @file_get_contents('https://www.google.com/search?source=lnms&sa=X&gbv=1&tbm=isch&q=' . $query);
+    if ($contents == false) {
+        echo 'Error, likely 404.';
+        echo "\n";
+    } else {
+        $dom = new DOMDocument();
+        $dom->loadHTML($contents);
+        $dom->preserveWhiteSpace = false;
+        $table = $dom->getElementsByTagName('table')[2];
+        $imgs = $table->getElementsByTagName('img');
+        $imglist = [];
+        foreach ($imgs as $img) {
+            array_push($imglist, $img->getAttribute('src'));
+        }
+        return $imglist;
+    }
+}
+foreach(searchImage('Test') as $img) {
+    echo '<img src="' . htmlspecialchars($img) . '">';
+}
