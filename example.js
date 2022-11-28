a666d8272627f3c4eddda8da6cbac971159bb8f6
mrfakename <me@mrfake.name>
2022-11-28 13:14:43 -0800

    No need for a CORS proxy while using NodeJS!

--- example.js
+++ example.js
@@ -11,7 +11,7 @@ function searchImage(query) {
         }),
     }
     return new Promise(function (resolve, reject) {
-        fetch("https://cors-anywhere.herokuapp.com/https://www.google.com/search?source=lnms&sa=X&gbv=1&tbm=isch&q=" + query, options)
+        fetch("https://www.google.com/search?source=lnms&sa=X&gbv=1&tbm=isch&q=" + query, options)
             .then((res) => res.text())
             .then((res) => {
                 var p = new jsdom.JSDOM(res);
